fba3d02eb51036ee99351c9b87dd3a6157b23390
FizaAli13 <fiza.ali1305@gmail.com>
2025-05-21 14:32:44 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
